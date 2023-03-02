fb1c4b3c7639c8daa554f930f6c6cfcdccbc1c60
vance <iamvancenet@gmail.com>
2023-03-02 21:19:11 +0330

    Writing hash functions sha251 and sha1

--- hash-sky/main.py
+++ hash-sky/main.py
@@ -1,1 +1,18 @@
+import sys
+import hashlib
 
+def hash_sha256():
+    sha256 = hashlib.sha256(text_hash.encode())
+    print(sha256.hexdigest())
+def hash_sha1():
+    sha1 = hashlib.sha1(text_hash.encode())
+    print(sha1.hexdigest())
+
+
+hash_algorithm = str (sys.argv[1])
+text_hash = str(sys.argv[2])
+
+if hash_algorithm == "sha256" :
+    hash_sha256()
+elif hash_algorithm == "sha1" :
+    hash_sha1()
